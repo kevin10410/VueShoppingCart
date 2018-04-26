2f0ae4012b3d7fcb0f7091bc8cc0adf064676afd
Kevin Wang (GIS) <kevin10410@gmail.com>
2018-04-26 23:31:14 +0800

    fix router

--- src/router/routes.js
+++ src/router/routes.js
@@ -3,8 +3,8 @@ import Detail from '../components/Detail.vue';
 import ShoppingCart from '../components/ShoppingCart.vue';
 
 export const routes = [
-    { path: '/', redirect: '/VueShoppingCart/page/1'},
-    { path: '/VueShoppingCart/page/:id', component: Products},
-    { path: '/VueShoppingCart/products/:id', component: Detail},
-    { path: '/VueShoppingCart/cart', component: ShoppingCart},
+    { path: '/', redirect: '/page/1'},
+    { path: '/page/:id', component: Products},
+    { path: '/products/:id', component: Detail},
+    { path: '/cart', component: ShoppingCart},
 ]
